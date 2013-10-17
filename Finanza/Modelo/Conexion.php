b80eb75e72fd8e61c87f1eee69c095541af8a587
amartineza <amartineza@JPII011.ancora.cl>
2013-10-17 16:48:03 -0300

    Modificación 17-10-2013 Implementación de "Modulos"-> usuarios

---
+++ Finanza/Modelo/Conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Conección
+ *
+ * @author amartineza
+ */
+class Conexión {
+    //put your code here
+}
+
+?>
